e9210212238b23badde9f66cfd1b4577df5364fc
MiguelMPicinin <miguelpicinin6@gmail.com>
2025-12-05 10:30:52 -0300

    05/12/25

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyBVO4W-bb4EH0b6YUpyqk7iAgssY8Thfvs",
+    authDomain: "pet-loc-novo.firebaseapp.com",
+    projectId: "pet-loc-novo",
+    storageBucket: "pet-loc-novo.firebasestorage.app",
+    messagingSenderId: "967703795061",
+    appId: "1:967703795061:web:20bf24a99f45a0507cef3c"
+};
